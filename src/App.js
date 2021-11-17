d0f8e2a42524a78e52be122f0078974f55f1e3b1
marmon179 <marmon179@gmail.com>
2021-11-17 21:13:34 +0300

    state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,44 @@ import "./App.css";
 import initialData from "./initialData"
 import Column from "./Column";
 import {DragDropContext} from "react-beautiful-dnd";
+import {useState} from "react";
 
 const App = () => {
 
-    const state = initialData
+    const [state, setState] = useState(initialData)
 
     const onDragEnd = (result) => {
+        const {destination, source, draggableId} = result;
+        if (!destination) {
+            return;
+        }
 
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
+        const column = state.columns[source.droppableId];
+        const newTaskIds = Array.from(column.taskIds);
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+
+        const newColumn = {
+            ...column,
+            taskIds: newTaskIds,
+        };
+        const newState = {
+            ...state,
+            columns: {
+                ...state.columns,
+                [newColumn.id]: newColumn,
+            },
+        };
+        setState(newState);
     }
     return (
-        <DragDropContext onDragEnd={onDragEnd()}>
+        <DragDropContext onDragEnd={onDragEnd}>
             {state.columnOrder.map(columnId => {
                 const column = state.columns[columnId]
                 const tasks = column.taskIds.map(taskId => state.task[taskId])
